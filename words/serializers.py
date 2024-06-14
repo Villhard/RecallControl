ef65f65626af3f9e05feb9b4729a6492a5c50bbb
Victor Ryabov <victorwillhard@gmail.com>
2024-06-14 18:26:55 +0300

    chore: Remove words app serializers and views

--- words/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from words.models import Word
-
-
-class WordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Word
-        fields = ("word", "definition", "example")
--- words/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from words.models import Word
-from words.serializers import WordSerializer
-
-
-class WordViewSet(viewsets.ModelViewSet):
-    queryset = Word.objects.all()
-    serializer_class = WordSerializer
