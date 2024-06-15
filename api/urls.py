dfd4c5e830930605ae01a5c6329a3844595dcf5b
Victor Ryabov <victorwillhard@gmail.com>
2024-06-15 03:45:21 +0300

    chore: Update API router to use DefaultRouter instead of SimpleRouter

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from api.views import WordViewSet
 
 
 
-router_v1 = SimpleRouter()
+router_v1 = DefaultRouter()
 router_v1.register("words", WordViewSet)
 
 urlpatterns = [
-    path("", include(router_v1.urls))
+    path("v1/", include(router_v1.urls))
 ]
