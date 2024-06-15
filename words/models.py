835be7a6d38c1926ef9c807aa5fb5bc3eca5045d
Victor Ryabov <victorwillhard@gmail.com>
2024-06-15 04:03:45 +0300

    feat: Update migrations for users and words apps

--- words/models.py
+++ words/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Word(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="words")
     word = models.CharField(max_length=100)
     definition = models.TextField()
     example = models.TextField()
