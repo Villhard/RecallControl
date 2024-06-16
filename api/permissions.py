edffa0c14ad6bb1fd19031e87eef82e54fa40a97
Victor Ryabov <victorwillhard@gmail.com>
2024-06-16 14:18:52 +0300

    fix: Update IsOwner permission to use IsAuthenticated base class

--- api/permissions.py
+++ api/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+class IsOwner(permissions.IsAuthenticated):
+    def has_object_permission(self, request, _, obj):
         return obj.owner == request.user
